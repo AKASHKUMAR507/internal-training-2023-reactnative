816bfdbabef6c18bd85b47946aad56f50410733f
AKASHKUMAR507 <akashkumar32755@gmail.com>
2023-05-08 17:53:54 +0530

    second commit

--- CryptoWaterCoin/src/screen/SignUpScreen.js
+++ CryptoWaterCoin/src/screen/SignUpScreen.js
@@ -1,11 +1,62 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { IMAGES } from '../assets/thems'
+import PhoneInput from 'react-native-phone-input';
 
 const SignUpScreen = () => {
   return (
-    <View>
-      <Text>SignUpScreen</Text>
-    </View>
+    <SafeAreaView>
+
+      <View>
+        <Image source={IMAGES.cryptoWater}/>
+      </View>
+
+      <View>
+        <Text>Create Account</Text>
+        <Text>Please Enter Register Details</Text>
+      </View>
+
+      <View>
+        <View>
+          <Text>First Name</Text>
+          <TextInput
+            placeholder='name'
+          />
+        </View>
+        <View>
+          <Text>First Name</Text>
+          <TextInput
+            placeholder='name'
+          />
+        </View>
+        <View>
+          <Text>First Name</Text>
+          <TextInput
+            placeholder='name'
+          />
+        </View>
+        <View>
+          <Text>Phone</Text>
+          {/* <PhoneInput 
+            placeholder={"Phone"}
+          /> */}
+        </View>
+        <View>
+          <Text>First Name</Text>
+          <TextInput 
+            placeholder='name'
+          />
+        </View>
+        <View>
+          <Text>First Name</Text>
+          <TextInput
+            placeholder='name'
+          />
+        </View>
+      </View>
+
+    </SafeAreaView>
   )
 }
 
